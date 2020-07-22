f0fa1c2598e09af71fe6037b7c96b32f3b27d8a4
snehan <snehanreddy2018>
2020-07-22 21:18:25 +0530

    updated

--- src/app/Features/login/login-routing.module.ts
+++ src/app/Features/login/login-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login.component';
 
-const routes: Routes = [{ path: 'login', component: LoginComponent,data :{ Title:'' ,ShowHeader:false,ShowNavBar:false} }];
+const routes: Routes = [{ path: '', component: LoginComponent,data :{ Title:'',ShowHeader:false,ShowNavBar:false} }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
